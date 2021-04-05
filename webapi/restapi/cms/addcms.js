b25554de8f5562e9fafe266a2e15f8ae0a952e28
Capslock-inc <capslock0408@gmail.com>
2021-04-05 18:09:32 +0530

    Answered

--- webapi/restapi/cms/addcms.js
+++ webapi/restapi/cms/addcms.js
@@ -1,6 +1,23 @@
+//importing
 const express = require('express');
 const router = express.Router();
+const sqlcon = require('../../sqlcon')
+
+//get requests
+
+router.get("/addcmsdata", (req,res) => {
+    res.send("addcmsapi")
+})
+
+//post requests
 
 
+router.post("/addcmsdata" , (req,res) => {
+    console.log(req.body);
+    res.status(200).send({"message":"data recieved -- addcms"})
+    let cmsdatafromangular = JSON.stringify(req.body);
+    let stored_procedure_query = "EXEC SpCmsData @cmsjson ='"+cmsdatafromangular+"';";
+    sqlcon.dosomthing(stored_procedure_query);
+})
 
 module.exports = router;
